aa3539d10fee5bc1b0159066d9f8399f81747d15
Bram Versluis <bram@backbase.com>
2017-07-20 19:28:51 +0200

    Added bootstrap scss files, added nix-genx.scss, added gulp-sass and a build script to build the sass files

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@
 
 var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
-
+var gulpSass = require('gulp-sass');
 
 gulp.task('build', [
-    'build-html'
+    'build-html',
+    'build-sass',
 ]);
 
 
@@ -17,4 +18,10 @@ gulp.task('build-html', function() {
             path: ['src/components/']
         }))
         .pipe(gulp.dest('build/'))
+});
+
+gulp.task('build-sass', function() {
+    return gulp.src('src/assets/stylesheets/**/*.scss')
+        .pipe(gulpSass().on('error', gulpSass.logError))
+        .pipe(gulp.dest('build/assets/css/'))
 });
